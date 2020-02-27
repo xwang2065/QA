842e183708cc566b8546415446a9bc0aae4a0f0c
Xiaoyang Wang <xwang2065@gmail.com>
2020-02-27 13:03:30 -0500

    Added comments for each method of the test class to explain why selected each input for my unit cases

--- XWAssignment2.Tests/TriangleSolverTest.cs
+++ XWAssignment2.Tests/TriangleSolverTest.cs
@@ -10,6 +10,10 @@ namespace XWAssignment2.Tests
     [TestFixture]
     public class TriangleSolverTest
     {
+        /// <summary>
+        /// First situation that cannot make a triangle,
+        /// which is the longest side is greater than sum of the other two sides
+        /// </summary>
         [Test]
         public void Analyze_Given1And2And4_ResultIsNoTriangle()
         {
@@ -23,6 +27,10 @@ namespace XWAssignment2.Tests
             Assert.AreEqual("no triangle", result);
         }
 
+        /// <summary>
+        /// Second situation that cannot make a triangle
+        /// which is the longest side equals to sum of the other two sides
+        /// </summary>
         [Test]
         public void Analyze_Given4And2And2_ResultIsNoTriangle()
         {
@@ -36,6 +44,11 @@ namespace XWAssignment2.Tests
             Assert.AreEqual("no triangle", result);
         }
 
+        /// <summary>
+        /// If the three side can make a triangle,
+        /// The only situation that can make a Equilateral Triangle
+        /// which is the three sides are all same
+        /// </summary>
         [Test]
         public void Analyze_Given2And2And2_ResultIsEquilateralTriangle()
         {
@@ -49,6 +62,11 @@ namespace XWAssignment2.Tests
             Assert.AreEqual("equilateral triangle", result);
         }
 
+        /// <summary>
+        /// If the three side can make a triangle,
+        /// First situation that can make a Isosceles Triangle
+        /// which is the two shorter sides are same
+        /// </summary>
         [Test]
         public void Analyze_Given3And2And2_ResultIsIsoscelesTriangle()
         {
@@ -62,6 +80,11 @@ namespace XWAssignment2.Tests
             Assert.AreEqual("isosceles triangle", result);
         }
 
+        /// <summary>
+        /// If the three side can make a triangle,
+        /// Second situation that can make a Isosceles Triangle
+        /// which is the two longer sides are same if the three side can make a triangle
+        /// </summary>
         [Test]
         public void Analyze_Given2And1And2_ResultIsIsoscelesTriangle()
         {
@@ -75,6 +98,11 @@ namespace XWAssignment2.Tests
             Assert.AreEqual("isosceles triangle", result);
         }
 
+        /// <summary>
+        /// If the three side can make a triangle,
+        /// The only situation that can make a Scalene Triangle
+        /// which is the three side are all different 
+        /// </summary>
         [Test]
         public void Analyze_Given24And16And20_ResultIsScaleneTriangle()
         {
